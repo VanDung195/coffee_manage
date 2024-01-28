bb683be9c29a7ceac2955be6865b43fbfd14a49f
HVD <hovandung23214@gmail.com>
2024-01-28 12:42:33 +0700

    Update InvoiceController.php

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -5,8 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\Invoice;
 use App\Http\Requests\StoreInvoiceRequest;
 use App\Http\Requests\UpdateInvoiceRequest;
+use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
-    
+    public function store(Request $request)
+    {
+        return $request->all();
+    }
 }
