a5e6ba5b7d353149661c202f103cd0d248c1686f
HVD <hovandung23214@gmail.com>
2024-01-31 12:59:36 +0700

    Update InvoiceController.php

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Invoice\StoreRequest;
 use App\Models\Invoice;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
-    public function store(Request $request)
+    use ResponseTrait;
+    public function store(Request $request): JsonResponse
     {
         try {
             
+            return $this->successResponse(1);
         } catch (\Throwable $th) {
             dd($th);
         }
