368d6b31116e90cb6dbd0ecacde16b7a950a8b94
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-02-23 10:54:59 +0700

    Update TestController.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,17 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Enums\TableIsPaidEnum;
-use App\Models\MenuItem;
 use App\Models\Table;
+use App\Models\MenuItem;
 use Illuminate\Http\Request;
+use App\Enums\TableIsPaidEnum;
+use App\Http\Controllers\TableController;
+use App\Models\Attendance;
 
 class TestController extends Controller
 {
     public function test()
     {
-        // $items = MenuItem::query()
-        //             ->where('name', 'like', '%' . 'Sinh tố' . '%')->get();
+        $item = MenuItem::query();
+        $items = MenuItem::query()
+                    ->where('name', 'like', '%' . 'Sinh tố' . '%')->get();
         // // dd($items);
         // foreach ($items as $key => $value) {
         //     // dd($value->name, $value->id);
@@ -28,7 +31,7 @@ class TestController extends Controller
         // return Table::query()->get('name');
     }
     public function create() {
-        return view('test');
+        return view('testee');
     }
     public function test2(Request $request)
     {
