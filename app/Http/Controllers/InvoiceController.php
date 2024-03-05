d2e86c19a416205533339e4e0f7c6357bc96a7a4
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-03-05 13:35:58 +0700

    Update InvoiceController.php

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -14,25 +14,54 @@ class InvoiceController extends Controller
     public function store(Request $request): JsonResponse
     {
         try {
-            $tableId = $request->input('table-id');    
-            $allData = $request->all();
-            $invoice_detail = [];
+            if($request->is_paid == 1) {
+                $tableId = $request->input('table-id');    
+                $allData = $request->all();
+                $invoice_detail = [];
 
-            //Lấy danh sách các tên món có trong hoá đơn
-            $ItemNames = $allData['name'];
+                //Lấy danh sách các tên món có trong hoá đơn
+                $ItemsId = $allData['id'];
 
-            //Lấy thông tin chi tiết của sản phấm 
-            $menuItems = MenuItem::query()
-                        ->whereIn('id',$ItemNames)->get();
+                //Lấy thông tin chi tiết của sản phấm 
+                $menuItems = MenuItem::query()
+                            ->whereIn('id',$ItemsId)->get();
 
-            $menuItemsMap = $menuItems->keyBy('name')->toArray();
+                $menuItemsMap = $menuItems->keyBy('id')->toArray();
+                /*Ví dụ: 
+                array:2 [ // app\Http\Controllers\InvoiceController.php:45
+                    1 => array:4 [
+                        "id" => 1
+                        "menu_category_id" => 1
+                        "name" => "Sinh tố dâu"
+                        "price" => 25.5
+                    ]
+                    2 => array:4 [
+                        "id" => 2
+                        "menu_category_id" => 1
+                        "name" => "Sinh tố lúa mạch"
+                        "price" => 15.235
+                    ]
+                ]
+                */
+                // dd($menuItemsMap);
+                foreach ($ItemsId as $index => $id) {
+                    // dd($index,$id);
+                    /*
+                    0 // app\Http\Controllers\InvoiceController.php:47
+                    "2" // app\Http\Controllers\InvoiceController.php:47*/
+                    $quantity = $allData['quantity'][$index];
 
-            // dd($menuItemsMap);
-            foreach ($ItemNames as $index => $name) {
-                $quantity = $allData['quantity'][$index];
+                    $price = isset($menuItemsMap[$id]['price']) ? $menuItemsMap[$id]['price'] : 0;
+                    // dd((float)$price);
 
+                    
+
+                    // dd($quantity, $price);
+                }
+                return $this->successResponse();
             }
-            return $this->successResponse();
+
+
         } catch (\Throwable $th) {
             dd($th);
         }
