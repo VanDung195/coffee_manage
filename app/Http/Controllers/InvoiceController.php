41e7f0891157dd32d1aa7f8f25a6ad4ee3347bec
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-03-09 10:20:32 +0700

    Update InvoiceController.php

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -17,79 +17,74 @@ class InvoiceController extends Controller
     public function store(StoreRequest $request): JsonResponse
     {
         try {
-            if($request->is_paid == 1) {
-                $tableId = $request->input('table-id');    
-                $allData = $request->all();
-                $invoice_detail = [];
-
-                //Lấy danh sách các tên món có trong hoá đơn
-                $ItemsId = $allData['id'];
-                // dd($allData);
-                //Lấy thông tin chi tiết của sản phấm (Hàm whereIn để tìm ra cái id của 1 mảng)
-                $menuItems = MenuItem::query()
-                            ->whereIn('id',$ItemsId)->get();
-                $menuNames = MenuItem::query()->whereIn('id',$ItemsId)->pluck('name');
-                // dd($menuNames, $menuItems);
-                $menuItemsMap = $menuItems->keyBy('id')->toArray();
-                // dd($menuItemsMap);
-                /*Ví dụ: 
-                array:2 [ // app\Http\Controllers\InvoiceController.php:45
-                    1 => array:4 [
-                        "id" => 1
-                        "menu_category_id" => 1
-                        "name" => "Sinh tố dâu"
-                        "price" => 25.5
-                    ]
-                    2 => array:4 [
-                        "id" => 2
-                        "menu_category_id" => 1
-                        "name" => "Sinh tố lúa mạch"
-                        "price" => 15.235
-                    ]
+            $tableId = $request->input('table-id');    
+            $allData = $request->all();
+            //Lấy danh sách các tên món có trong hoá đơn
+            $ItemsId = $allData['id'];
+            // dd($allData);
+            //Lấy thông tin chi tiết của sản phấm (Hàm whereIn để tìm ra cái id của 1 mảng)
+            $menuItems = MenuItem::query()
+                        ->whereIn('id',$ItemsId)->get();
+            $menuNames = MenuItem::query()->whereIn('id',$ItemsId)->pluck('name');
+            // dd($menuNames, $menuItems);
+            $menuItemsMap = $menuItems->keyBy('id')->toArray();
+            // dd($menuItemsMap);
+            /*Ví dụ: 
+            array:2 [ // app\Http\Controllers\InvoiceController.php:45
+                1 => array:4 [
+                    "id" => 1
+                    "menu_category_id" => 1
+                    "name" => "Sinh tố dâu"
+                    "price" => 25.5
                 ]
-                */
-                // dd($menuItemsMap);
-                $total_price = 0;
-                foreach($ItemsId as $index => $id) {
-                    // dd((int)$id);
-                    $quantity = $allData['quantity'][$index];
-                    $price = $menuItemsMap[$id]['price'];
-
-                    $total_price += $quantity * $price;
-                }
-                // dd($total_price);
-                //cách cũ
-                // check if current date is between two dates php
-                // $currentDate = date('Y-m-d');
-
-                // $endDate = $this->end_date->format('Y-m-d');
-                // $startDate = $this->start_date->format('Y-m-d');
-                // //echo $paymentDate; // echos today! 
-                    
-                // // if (($currentDate >= $this->end_date) && ($currentDate <= $this->start_date)){
-                // //     return true;s
-                // // }
-
-                // // return false;
-                // // dd($endDate);
-                // // dd($this->$startDate);
-                // return ($currentDate <= $endDate) || ($currentDate >= $startDate);
-                // $date = date('Y-m-d');
-                // $time = date('H:i:s');
-                // dd($date, $time);
-                // dd($total_price);
-                // $dt = new DateTime();
+                2 => array:4 [
+                    "id" => 2
+                    "menu_category_id" => 1
+                    "name" => "Sinh tố lúa mạch"
+                    "price" => 15.235
+                ]
+            ]
+            */
+            // dd($menuItemsMap);
+            $total_price = 0;
+            foreach($ItemsId as $index => $id) {
+                // dd((int)$id);
+                $quantity = $allData['quantity'][$index];
+                $price = $menuItemsMap[$id]['price'];
+                $total_price += $quantity * $price;
+            }
+            // dd($total_price);
+            //cách cũ
+            // check if current date is between two dates php
+            // $currentDate = date('Y-m-d');
+            // $endDate = $this->end_date->format('Y-m-d');
+            // $startDate = $this->start_date->format('Y-m-d');
+            // //echo $paymentDate; // echos today! 
                 
-                $now = Carbon::now('Asia/Bangkok');
+            // // if (($currentDate >= $this->end_date) && ($currentDate <= $this->start_date)){
+            // //     return true;s
+            // // }
+            // // return false;
+            // // dd($endDate);
+            // // dd($this->$startDate);
+            // return ($currentDate <= $endDate) || ($currentDate >= $startDate);
+            // $date = date('Y-m-d');
+            // $time = date('H:i:s');
+            // dd($date, $time);
+            // dd($total_price);
+            // $dt = new DateTime();
+            
+            $now = Carbon::now('Asia/Bangkok');
+            if((int)$request->is_paid == 1) {
+                
                 // dd($now->format('H:i:s'));
-                // $invoice = Invoice::create([
-                //     'created_at' => $now->format('d-m-Y'),
-                //     'checkin_time' => $now->format('H:i:s'),
-                //     'checkout_time' => $now->format('H:i:s'),
-                //     'total_price' => $total_price,
-                // ]);
-                // $invoice_id = $invoice->id;
-                $invoice_id = 1;
+                $invoice = Invoice::create([
+                    'created_at' => $now->format('Y:m:d'),
+                    'checkin_time' => $now->format('H:i:s'),
+                    'checkout_time' => $now->format('H:i:s'),
+                    'total_price' => $total_price,
+                ]);
+                $invoice_id = $invoice->id;
                 foreach ($ItemsId as $index => $id) {
                     // dd($index,$id);
                     /*
@@ -103,11 +98,11 @@ class InvoiceController extends Controller
                     $item = MenuItem::query()
                             ->where('id', $id)->first();
 
-                    // InvoiceDetail::create([
-                    //     'invoice_id' => $invoice_id,
-                    //     'menu_item_id' => $id,
-                    //     'quantity' => $quantity,
-                    // ]);
+                    InvoiceDetail::create([
+                        'invoice_id' => $invoice_id,
+                        'menu_item_id' => $id,
+                        'quantity' => $quantity,
+                    ]);
                     
                     $invoice_details[] = [
                         'id' => $id,
@@ -117,6 +112,7 @@ class InvoiceController extends Controller
                     ];
                     // dd($quantity, $price);
                 }
+                $message = 'Thanh cong roi nhe!';
                 return $this->successResponse([
                     'table_id' => $tableId,
                     'invoice_details' => $invoice_details,
@@ -124,8 +120,21 @@ class InvoiceController extends Controller
                     'created_at' => $now->format('d:m:Y'),
                     'checkin_time' => $now->format('H:i'),
                     'checkout_time' => $now->format('H:i'),
-                ]);
+                    'is_paid' => $request->is_paid,
+                ], $message);
             }
+
+            //is_paid ===  0 (chua thanh toan thi se them vao session de lo khach hang co huy hoa don)
+            if(!session()->has('invoice'))
+            {
+                session()->put('invoice', []);
+            }
+            
+            $invoice = session()->get('invoice');
+            if(array_key_exists($tableId, $invoice)){
+                dd(1);
+            }
+
             return $this->successResponse();
 
         } catch (\Throwable $th) {
