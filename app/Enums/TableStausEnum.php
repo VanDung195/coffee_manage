1d05b7328761370cf2dd68f932491c457285250b
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-03-28 14:26:44 +0700

    Create TableStausEnum.php

---
+++ app/Enums/TableStausEnum.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static OptionOne()
+ * @method static static OptionTwo()
+ * @method static static OptionThree()
+ */
+final class TableStausEnum extends Enum
+{
+    public const BAN = 0;
+    public const TRONG = 1;
+
+}
