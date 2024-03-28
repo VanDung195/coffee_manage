3e336a318cc2aaa56fcdc9e66a5f81cea4a7063d
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-03-28 14:26:52 +0700

    Update InvoiceApiController.php

--- app/Http/Controllers/InvoiceApiController.php
+++ app/Http/Controllers/InvoiceApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Invoice;
+use App\Models\Table;
 use Illuminate\Http\Request;
 
 class InvoiceApiController extends Controller
@@ -10,9 +11,25 @@ class InvoiceApiController extends Controller
     use ResponseTrait;
     private object $model;
 
-    public function __construct()
+    // public function __construct()
+    // {
+    //     $this->model = Invoice::query();
+    // }
+    public function index()
     {
-        $this->model = Invoice::query();
+        $table_invoice_id = Table::query()
+                                    ->where('invoice_id', '<>', 0)
+                                    ->pluck('invoice_id')->toArray();
+        $invoices = Invoice::with(['details' => function($query) {
+            $query->select('invoice_id', 'menu_item_id', 'quantity');
+        }, 'details.menuItems' => function($query) {
+            $query->select('id', 'name');
+        }])
+        ->whereIn('id', $table_invoice_id)
+        ->get()
+        ->toArray();
+        $message = 'Get api thanh cong';
+        return $this->successResponse($invoices,$message);
     }
     
 }
