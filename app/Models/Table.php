a9c8ac5bd71c9a4d6a87ca53e6bf794ba73c16fd
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-03-29 11:23:18 +0700

    Update Table.php

--- app/Models/Table.php
+++ app/Models/Table.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Table extends Model
 {
     use HasFactory;
     public $timestamps = false;
+    
+    protected $fillable = ['invoice_id'];
+
+    public function invoice(): BelongsTo
+    {
+        return $this->belongsTo(Invoice::class, 'id');
+    }
 }
