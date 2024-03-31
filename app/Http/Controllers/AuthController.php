24ed915bd11da9acfe4fdea232898d0a45c97044
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-03-31 22:52:56 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login(){
+    public function login()
+    {
         return view('auth.login');
     }
-    
+    public function process_login(Request $request) 
+    {
+        $account = $request->account;
+        $password = $request->password;
+        
+        $user = User::query()
+                ->where('account','=', $account)
+                ->first();
+        if($user) {
+            $user = User::query()
+                    ->where('password', $password)->first();
+        }
+        dd($user);
+    }
 }
