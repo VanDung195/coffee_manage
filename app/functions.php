3d451f5491aff24d66a81322111b9c95c3f07e06
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-03-31 22:52:53 +0700

    update

--- app/functions.php
+++ app/functions.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Enums\UserRoleEnum;
+
 if(!function_exists('getAndCacheMenuItems')){
     function getAndCacheMenuItems()
     {
         
+    }
+}
+if(!function_exists('user')) {
+    function user(){
+        return auth()->user();
+    }
+}
+
+if(!function_exists('isAdmin')) {
+    function isAdmin(){
+        return user() && user()->role === UserRoleEnum::ADMIN;
     }
 }
