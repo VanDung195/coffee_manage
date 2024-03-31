f61694c7892a960208d6083aa2a2cf510b128a94
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-03-31 13:42:03 +0700

    update (update: status, invoice_id)

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Enums\TableIsPaidEnum;
+use App\Enums\TableStausEnum;
 use App\Models\MenuItem;
 use App\Models\Table;
+use Illuminate\Http\Request;
 
 class TableController extends Controller
 {
@@ -16,14 +18,24 @@ class TableController extends Controller
         $items = MenuItem::query()
                     ->get();
         foreach ($tables as $table) {
-            printf($table->name);
+            
         }
-        dd();
+        // dd();
         $is_paids = TableIsPaidEnum::getKeys();
         return view('admin.index',[ 
             'tables' => $tables,
             'items' => $items,
             'is_paids' => $is_paids,
         ]);
+    }
+    public function update(Request $request) 
+    {
+        Table::query()
+        ->where('name',$request->table_name)
+        ->update([
+            'status' => TableStausEnum::getKey(1),
+            'invoice_id' => 0,
+        ]);
+        return 1;
     }
 }
