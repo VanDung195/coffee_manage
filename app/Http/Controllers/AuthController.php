254d1274d0263640bcd25d46aa22e7b2cfc4fe9a
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-04-01 13:13:24 +0700

    update (register)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\UserRoleEnum;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -24,5 +26,35 @@ class AuthController extends Controller
                     ->where('password', $password)->first();
         }
         dd($user);
+    }
+    public function register() {
+        $roleForRegister = UserRoleEnum::getRoleForRegister();
+        return view('auth.register', [
+            'roles' => $roleForRegister,
+        ]);
+    }
+    public function process_register(Request $request)
+    {   
+        // dd($request->all());
+        $user = User::query()
+                ->where('account', $request->account)->first();
+
+        if(isset($user)) {
+            return redirect()->route('login')->with('error', 'trung');
+        }
+
+        $password = Hash::make($request->password);
+        if(is_null($user))
+        {
+            $user = new User();
+            $user->name = $request->name;
+            $user->account = $request->account;
+            $user->password = $password;
+            $user->gender = 1;
+            $user->phone = $request->account;
+            $user->role = $request->role;
+        }
+        $user->save();
+        return redirect()->route('table');
     }
 }
