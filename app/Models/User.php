8f8e5df09562a4830e2851b53ed81427ea888d90
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-04-01 22:27:17 +0700

    update (user authenticatableTrait and implements)

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\Authenticatable as AuthenticatableTrait;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class User extends Model implements AuthenticatableContract
 {
+    use AuthenticatableTrait;
     use HasFactory;
 }
