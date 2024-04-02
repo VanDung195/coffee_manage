4081b55fdf38912fea91a1037e3d1948649fb552
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-04-02 19:49:29 +0700

    update (route: admin)

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
@@ -35,6 +36,11 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::prefix('admin')
+                ->name('admin.')
+                ->middleware(AdminMiddleware::class)
+                ->group(base_path('route/admin.php')); 
         });
     }
 }
