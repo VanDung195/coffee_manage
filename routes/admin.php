d0d53b6068411c06932b58e15421bffdb294aa2f
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-04-03 22:05:24 +0700

    Create admin.php

---
+++ routes/admin.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\TestController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/test', [TestController::class, 'test']);
+Route::get('/user', [UserController::class, 'index'])->name('user.index');
+Route::get('/{user}', [UserController::class, 'show'])->name('user.show');
+// Route::group([
+//     'as' => 'users',
+//     'prefix' => 'users'],
+//     function() {
+        
+//     }
+// );
