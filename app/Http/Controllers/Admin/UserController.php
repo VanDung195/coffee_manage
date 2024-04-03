98b14bd903f5257611cd70ccff505d9c2a355349
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-04-03 22:05:09 +0700

    Create UserController.php

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View as FacadesView;
+
+class UserController extends Controller
+{
+    protected object $model;
+    protected string $table;
+    
+    public function __construct()
+    {
+        $this->model = User::query();
+        $this->table = (new User())->getTable();
+        FacadesView::share('title', ucwords($this->table));  
+        FacadesView::share('table', $this->table);
+    }
+
+    public function index() {
+        $users = User::query()->paginate();
+
+        return view('admin.user.index', [
+            'users' => $users,
+        ]);
+    } 
+    public function show($user) {
+        dd(1);
+    }
+}
