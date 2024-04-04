dfa20fc72ce226e2dc5090ec46c94c9f973ef653
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-04-04 11:50:44 +0700

    Update admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/test', [TestController::class, 'test']);
 Route::get('/user', [UserController::class, 'index'])->name('user.index');
-Route::get('/{user}', [UserController::class, 'show'])->name('user.show');
+Route::get('/user/{user}', [UserController::class, 'show'])->name('user.show');
 // Route::group([
 //     'as' => 'users',
 //     'prefix' => 'users'],
