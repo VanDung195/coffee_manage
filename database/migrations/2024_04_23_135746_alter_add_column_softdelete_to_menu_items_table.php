04fdd002d940b7e26103ac54e0d303d6eeeda0c3
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-04-23 21:59:32 +0700

    Create 2024_04_23_135746_alter_add_column_softdelete_to_menu_items_table.php

---
+++ database/migrations/2024_04_23_135746_alter_add_column_softdelete_to_menu_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('menu_items', function (Blueprint $table) {
+            if(!Schema::hasColumn('menu_items', 'deleted_at')) {
+                $table->softDeletes(); 
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('menu_items', function (Blueprint $table) {
+            if(Schema::hasColumn('menu_items', 'deleted_at')) {
+                $this->dropColumn('deleted_at');
+            }
+        });
+    }
+};
