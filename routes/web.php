2777c37e5fecb3e9f9f69c3c479f16929aca1d3d
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-05-28 22:44:50 +0700

    update (add middleware and new route)

--- routes/web.php
+++ routes/web.php
@@ -28,13 +28,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/test', [TestController::class, 'test']);
 Route::get('/test2', [TestController::class, 'test2'])->name('test2');
 Route::get('/test3', [TestController::class, 'test3']);
+Route::get('test_view', [TestController::class, 'test_view']);
+
 Route::get('/testApi', [InvoiceApiController::class, 'index']);
 
 Route::get('/api', [InvoiceApiController::class, 'index'])->name('api.invoices');
 Route::get('/update', [TableController::class, 'update'])->name('table.update');
 
 // Route::get('/index',[TableController::class, 'index'])->name('table')->middleware(AdminMiddleware::class);
-Route::get('/index',[TableController::class, 'index'])->name('table');
+Route::get('/index',[TableController::class, 'index'])->name('table')->middleware(AdminMiddleware::class);
 
 Route::get('/', [AuthController::class, 'login'])->name('login');
 Route::get('/login', [AuthController::class, 'login'])->name('login');
@@ -46,7 +48,8 @@ Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/search', [MenuItemController::class, 'search'])->name('item.search');
 
-Route::post('/6635bb17c246e2',[InvoiceController::class, 'store'])->name('invoice.store');
+Route::post('/6635bb17c246e2',[InvoiceController::class, 'store'])->name('invoice.store')->middleware(AdminMiddleware::class);
+Route::post('/table_update', [InvoiceController::class, 'table_update'])->name('table_update')->middleware(AdminMiddleware::class);
 Route::post('/store_qr', [InvoiceController::class, 'store_qr'])->name('invoice.store_qr');
 Route::get('/update_tot', [InvoiceController::class, 'update'])->name('invoice.update');
 
@@ -55,5 +58,3 @@ Route::get('/create', [TestController::class, 'create']); //test
 Route::get('/attendance', [AttendanceController::class, 'index'])->name('attendance.index');
 
 Route::get('/qr_order/{table_id}', [TableController::class, 'qr_show'])->name('qr.show');
-
-Route::get('test_view', [TestController::class, 'test_view']);
