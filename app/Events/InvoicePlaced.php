143503f4e4db63c827b75661d85b9a5efaf765af
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-06-30 21:48:32 +0700

    Update InvoicePlaced.php

--- app/Events/InvoicePlaced.php
+++ app/Events/InvoicePlaced.php
@@ -20,9 +20,13 @@ class InvoicePlaced implements ShouldBroadcast
     public $created_at;
     public $checkin_time;
     public $checkout_time;
+    public $customer_payment;
+    public $remaining_money;
     public $is_paid;
+    public $is_qr;
 
-    public function __construct($table_id, $details, $total_price, $created_at, $checkin_time, $checkout_time, $is_paid)
+
+    public function __construct($table_id, $details, $total_price, $created_at, $checkin_time, $checkout_time, $customer_payment, $remaining_money, $is_paid, $is_qr)
     {
         $this->table_id = $table_id;
         $this->details = $details;
@@ -30,7 +34,10 @@ class InvoicePlaced implements ShouldBroadcast
         $this->created_at = $created_at;
         $this->checkin_time = $checkin_time;
         $this->checkout_time = $checkout_time;
+        $this->customer_payment = $customer_payment;
+        $this->remaining_money = $remaining_money;
         $this->is_paid = $is_paid;
+        $this->is_qr = $is_qr;
     }
     // public function __construct($invoice_detail)
     // {
