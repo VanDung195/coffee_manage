d0d93b7a3492ffa3c0dd14d4f6fff85503951542
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-07-04 21:56:04 +0700

    Create 2024_07_04_042858_alter_add_column_description_to_shifts_table.php

---
+++ database/migrations/2024_07_04_042858_alter_add_column_description_to_shifts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('shifts', function (Blueprint $table) {
+            if(!Schema::hasColumn('shifts', 'description')) {
+                $table->string('description'); 
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('shifts', function (Blueprint $table) {
+            if(Schema::hasColumn('shifts', 'description')) {
+                $table->dropColumn('description'); 
+            }
+        });
+    }
+};
