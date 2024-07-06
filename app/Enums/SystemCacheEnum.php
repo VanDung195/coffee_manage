1d781cd6324beea6c7bb50a631b1216970070547
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-07-06 22:32:15 +0700

    Update SystemCacheEnum.php

--- app/Enums/SystemCacheEnum.php
+++ app/Enums/SystemCacheEnum.php
@@ -14,4 +14,5 @@ final class SystemCacheEnum extends Enum
     public const MENU_ITEMS = 'menu_items';
     public const TABLE_NAMES = 'table_names';
     public const AVAILABLE_TABLE_NAME = 'avalable_table_name';
+    public const SHIFT = 'shift';
 }
