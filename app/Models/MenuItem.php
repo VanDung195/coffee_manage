2fd4ebf15195718f08423f242ec9099a3469d506
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-07-11 21:34:59 +0700

    Update MenuItem.php

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -4,9 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class MenuItem extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'menu_category_id',
+        'name',
+        'price',
+        'deleted_at'
+    ];
+
+    public $timestamps = false;
+    public function getPriceVNDAttribute()
+    {
+        $price_vnd = number_format($this->price, 0, ',', '.');
+        return $price_vnd;
+    }
+    public function menu_category(): BelongsTo
+    {
+        return $this->belongsTo(MenuCategory::class, 'id');
+    }
 }
