98f3c0fa22dc3370499e3439cfaa4d5d351f30b9
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-07-11 21:34:30 +0700

    Update MenuItemController.php

--- app/Http/Controllers/MenuItemController.php
+++ app/Http/Controllers/MenuItemController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\MenuItem;
 use App\Http\Requests\StoreMenuItemRequest;
 use App\Http\Requests\UpdateMenuItemRequest;
+use App\Models\MenuCategory;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -12,23 +13,48 @@ class MenuItemController extends Controller
 {
     use ResponseTrait;
 
-    public function search(Request $request): JsonResponse
+    protected object $model;
+    protected string $table;
+
+    public function __construct()
     {
-        if($request->ajax())
-        {
-            $output = '';
-            $items = MenuItem::query()
-                    ->where('name', 'like', '%' . $request->search . '%')->get();
+        $this->model = MenuItem::query();
+        $this->table = (new MenuItem())->getTable();
+    }
+    // public function search(Request $request): JsonResponse
+    // {
+    //     if($request->ajax())
+    //     {
+    //         $output = '';
+    //         $items = MenuItem::query()
+    //                 ->where('name', 'like', '%' . $request->search . '%')->get();
             
-            if($items)
-            {
-                $output .= '<ul>';
-                foreach ($items as $key => $item) {
-                    $output .= '<li><a href="#" data-id="' . $item->id . '" class="item-link">' . $item->name . '</a></li>';
-                }
-                $output .= '</ul>';
-            }
+    //         if($items)
+    //         {
+    //             $output .= '<ul>';
+    //             foreach ($items as $key => $item) {
+    //                 $output .= '<li><a href="#" data-id="' . $item->id . '" class="item-link">' . $item->name . '</a></li>';
+    //             }
+    //             $output .= '</ul>';
+    //         }
+    //     }
+    //     return $this->successResponse($output);
+    // }
+    public function index(Request $request)
+    {
+        $selected_category = $request->get('category');
+        // $query = $this->model->
+        if(!is_null($selected_category))
+        {
+            dd(1);
         }
-        return $this->successResponse($output);
+        $menu_categories = MenuCategory::query()
+                    ->get();
+        $menu_items = MenuItem::query()
+                        ->get();
+        return view('admin.menu_item.index', [
+            'menu_items' => $menu_items,
+            'menu_categories' => $menu_categories,
+        ]);
     }
 }
