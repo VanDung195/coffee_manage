757defa2e08d5cef66043540168c5be3649fb7bc
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-07-14 22:00:15 +0700

    update (crud route)

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\StatisticController;
+use App\Http\Controllers\MenuItemController;
 use App\Http\Controllers\TestController;
 use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
@@ -9,6 +10,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/test', [TestController::class, 'test']);
 Route::get('/user', [UserController::class, 'index'])->name('user.index');
 Route::get('/user/{user}', [UserController::class, 'show'])->name('user.show');
+Route::put('/edit/{user_id}', [UserController::class, 'edit'])->name('user.edit');
+Route::put('/update', [UserController::class, 'update'])->name('user.update');
 // Route::group([
 //     'as' => 'users',
 //     'prefix' => 'users'],
@@ -34,4 +37,11 @@ Route::get('/user/{user}', [UserController::class, 'show'])->name('user.show');
     Route::get('/year', [StatisticController::class, 'statistic_year'])->name('statistic.year');
     Route::get('/statistic_date_range', [StatisticController::class, 'statistic_date_range_i'])->name('statistic.date_range_i');
     Route::get('/date_range', [StatisticController::class, 'statistic_date_range'])->name('statistic.range');
+
+    Route::get('/menu_items', [MenuItemController::class, 'index'])->name('menu_items.index');
+    Route::get('/create', [MenuItemController::class, 'create'])->name('menu_items.create');
+    Route::post('/store', [MenuItemController::class, 'store'])->name('menu_items.store');
+    Route::put('/edit/{menu_item}', [MenuItemController::class, 'edit'])->name('menu_items.edit');
+    Route::put('/update', [MenuItemController::class, 'update'])->name('menu_items.update');
+    Route::delete('/destroy', [MenuItemController::class, 'destroy'])->name('menu_items.destroy');
 // });
