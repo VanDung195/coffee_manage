887373781bb0308c46197ca1a67b05af6d2eabbc
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-07-17 21:28:29 +0700

    update (fillable and get salary fun)

--- app/Models/Position.php
+++ app/Models/Position.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Position extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+    protected $fillable = [
+        'name', 
+        'salary'
+    ];
+
+    public function getSalaryFormattedAttribute()
+    {
+        $salary = number_format($this->salary, 0, ',', '.');
+        return $salary;
+    }
+
+    public function getSalaryDividedAttribute()
+    {
+        $salary = $this->salary / 1000;
+        return $salary;
+    }
 }
