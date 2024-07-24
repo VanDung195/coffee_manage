b38b14544934e786aac7e521339739f8f10ab133
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-07-24 22:19:31 +0700

    update ajax delete and notifi

--- resources/views/admin/table/edit.blade.php
+++ resources/views/admin/table/edit.blade.php
@@ -4,34 +4,78 @@
     <div class="col-12">
         <div class="card">
             <div class="card-body">
-                <form class="form-horizontal" action="{{ route('admin.menu_items.update') }}" method="POST" id="form-create-menu-item">
+                <form class="form-horizontal" method="POST" id="form-create-menu-item">
                     @csrf
                     @method('put')
-                    <input type="hidden" name="menu_item_id" value="{{ $item->id }}">
+                    <input type="hidden" name="table_id" id="table-id" value="{{ $table->id }}">
                      <div class="form-row">
-                        <div class="form-group col-2">
-                            <label for="category">Loại món</label>
-                            <select name="category" id="category" class="form-control">
-                                @foreach ($categories as $category)
-                                    <option value="{{ $category->id }}" @if ((int)$category->id == $item->menu_category->id ) selected @endif>
-                                        {{ $category->name }} 
-                                    </option>
-                                @endforeach
+                        <div class="form-group col-1">
+                            <label for="">Tầng</label>
+                            <select name="floor" id="floor" class="form-control">
+                                <option value="1" @if ($table->floor == 1) selected @endif>1</option>
+                                <option value="2" @if ($table->floor == 2) selected @endif>2</option>
+                                <option value="3" @if ($table->floor == 3) selected @endif>3</option>
                             </select>
                         </div>
-                        <div class="form-group col-5">
-                            <label for="name">Tên món</label>
-                            <input type="text" name="name" id="menu-item-name" class="form-control" value="{{ $item->name }}">
-                        </div>
                         <div class="form-group col-2">
-                            <label for="price">Giá (Ví dụ: 10 = 10.000vnđ)</label>
-                            <input type="number" name="price" id="price" placeholder="Ví dụ: 1 = 10.000đ, 19 = 19.000đ" class="form-control" inputmode="numeric" value="{{ $item->price_for_edit }}">
+                            <label for="name">Tên bàn: (VD: T1_2)</label>
+                            <input type="text" name="name" id="name" class="form-control" value="{{ $table->name }}" required>
+                        </div>
+                        <div class="form-group">
+                            <button class="btn btn-submit btn-success" style="float: left; margin-top:27px; margin-left:10px;">Sửa bàn</button>
+                        </div>
+                        <div class="form-group">
+                            {{-- <button class="btn btn-danger" style="float: left; margin-top:27px;; margin-left:10px;">Huỷ</button> --}}
+                            <a class="btn btn-danger" style="float: left; margin-top:27px;; margin-left:10px;" href="{{ route('admin.tables.index') }}">Huỷ</a>
                         </div>
                     </div>
-                    <button class="btn btn-success" style="float: left;">Thêm món</button>
                 </form>
             </div>
         </div>
     </div>
 </div>
 @endsection
+@push('js')
+<script>
+    $(document).ready(function () {
+        $('.btn-submit').on('click', function(event){
+            event.preventDefault()
+            let name = $('#name').val();
+            let floor = $('#floor').val();
+            let table_id = $('#table-id').val();
+            $.ajax({
+                type: "put",
+                url: '{{ route('admin.tables.update') }}',
+                data: {
+                    name: name,
+                    floor: floor,
+                    table_id: table_id,
+            },
+                dataType: "json",
+                success: function (response) {
+                    console.log(response);
+                    $.toast({
+                        heading: 'Thành công',
+                        text: response.message,
+                        showHideTransition: 'slide',
+                        icon: 'success'
+                    })
+
+                    let routeUrl = '{{ route('admin.tables.index') }}';
+                    setTimeout(()=> {
+                        window.location.href = routeUrl;
+                    }, 3000);
+                },
+                error: function(error) {
+                    $.toast({
+                        heading: 'Lỗi',
+                        text: error.responseJSON.message,
+                        showHideTransition: 'fade',
+                        icon: 'error'
+                    })
+                }
+            });
+        })
+    });
+</script>
+@endpush
