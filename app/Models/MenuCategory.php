686b4efbc32c1b75abac66ad877b50a3aa338602
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-07-24 22:17:12 +0700

    update fillable

--- app/Models/MenuCategory.php
+++ app/Models/MenuCategory.php
@@ -5,17 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\SoftDeletes;
+// use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MenuCategory extends Model
 {
     use HasFactory;
-    use SoftDeletes;
+    // use SoftDeletes;
     
     public $timestamps = false;
 
     protected $fillable = [
-        'name'
+        'name',
+        'is_hidden'
     ];
     // public function menu_items(): HasMany
     // {
