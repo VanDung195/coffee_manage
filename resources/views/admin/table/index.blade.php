8a03992577e0f76c48b096b6f5ddeb28e155e7cb
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-07-24 22:19:46 +0700

    update ajax delete and notifi

--- resources/views/admin/table/index.blade.php
+++ resources/views/admin/table/index.blade.php
@@ -4,9 +4,7 @@
     <div class="col-12">
         <div class="card">
             <div class="card-header">
-                <a href="" class="btn btn-success">Thêm bàn</a>
-                <a href="" class="btn btn-success">Thêm bàn (takeaway)</a>
-                <a href="" class="btn btn-success">Thêm bàn (unknow)</a>
+                <a href="{{ route('admin.tables.create') }}" class="btn btn-success">Thêm bàn</a>
             </div>
             <div class="card-body">
                 <table class="table table-hover table-centered mb-0">
@@ -21,9 +19,9 @@
                     </thead>
                     <tbody>
                         @foreach ($tables as $table)
-                            <tr>
+                            <tr id="tr-table-id-{{ $table->id }}">
                                 <td>
-                                    <a>{{ $table->stt }}</a>
+                                    <a>{{ $table->id }}</a>
                                 </td>
                                 <td>
                                     <a>{{ $table->name }}</a>
@@ -32,14 +30,14 @@
                                     <a>{{ $table->floor }}</a>
                                 </td>
                                 <td>
-                                    <form action="" method="POST" style="margin: 0px;"> 
+                                    <form action="{{ route('admin.tables.edit', $table->id) }}" method="POST" style="margin: 0px;"> 
                                         @csrf
                                         @method('put')
                                         <button class="btn btn-success">Sửa bàn</button>
                                     </form>
                                 </td>
                                 <td>
-                                    <button data-pos-id="{{ $table->name }}" class="btn-delete btn btn-danger">Xoá bàn</button>
+                                    <button data-table-id="{{ $table->id }}" class="btn-delete btn btn-danger">Xoá bàn</button>
                                 </td>
                             </tr>
                         @endforeach
@@ -61,13 +59,13 @@
                 <form action="" method="POST" id="form-accept">
                     @csrf
                     @method('delete')
-                    <input type="hidden" name="pos_id" id="pos-id">
-                    <h4 class="center">Nếu bạn muốn xoá loại món này thì bạn phải xoá tất cả các món có liên quan đến loại món này. Tiếp tục xoá?</h4>
+                    <input type="hidden" name="table_id" id="table-id">
+                    <h4 class="center">Xác nhận?</h4>
                 </form>
             </div>
             <div class="modal-footer">
                 <button class="btn-cancel btn btn-danger">Huỷ</button>
-                <button class="btn-submit-form btn btn-success">Tôi đã xoá!</button>
+                <button class="btn-submit-form btn btn-success">Chấp nhận</button>
             </div>
         </div>
     </div>
@@ -76,17 +74,63 @@
 @push('js')
     <script>
         $('.btn-delete').on('click', function(){
-            let pos_id = $(this).data('pos-id');
-            $('#pos-id').val(pos_id);
+            let table_id = $(this).data('table-id');
+            $('#table-id').val(table_id);
             $('#modal-accept').modal('show');
+        })
+        $('.btn-cancel').on('click', function(){
+            $('#modal-accept').modal('toggle');
         })
         $(document).ready(function () {
             $('.btn-submit-form').click(function(){
-                $('#modal-accept').modal('toggle');
-                setTimeout(()=> {
-                    $('#form-accept').submit();
-                }, 400);
+                let table_id = $('#table-id').val();
+                $.ajax({
+                    type: "delete",
+                    url: '{{ route('admin.tables.destroy') }}',
+                    data: {
+                        table_id: table_id,
+                    },
+                    dataType: "json",
+                    success: function (response) {
+                        let id = response.data.id;
+                        $.toast({
+                            heading: 'Thành công',
+                            text: response.message,
+                            showHideTransition: 'slide',
+                            icon: 'success'
+                        })
+                        $('#tr-table-id-'+id).remove();
+                        $('#modal-accept').modal('toggle');
+                    },
+                    error: function(error) {
+                        $.toast({
+                            heading: 'Lỗi',
+                            text: error.responseJSON.message,
+                            showHideTransition: 'fade',
+                            icon: 'error'
+                        })
+                    }
+                });
+
             })
         });
+        function notifyError(error)
+        {
+            $.NotificationApp.send("Error",error,"bottom-left","red","Icon")
+        }
+        function notifySuccess(success)
+        {
+            $.NotificationApp.send("Success",success,"bottom-left","green","Icon")
+        }
+        @if(session('error'))
+            $(document).ready(function() {
+                notifyError("{{ session('error') }}");
+            });
+        @endif
+        @if(session('success'))
+            $(document).ready(function() {
+                notifySuccess("{{ session('success') }}");
+            });
+        @endif
     </script>
 @endpush
