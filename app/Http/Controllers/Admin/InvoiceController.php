6e2dd39f1a85b4f18b7319be944df6f138ac909f
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-07-27 22:49:22 +0700

    Update InvoiceController.php

--- app/Http/Controllers/Admin/InvoiceController.php
+++ app/Http/Controllers/Admin/InvoiceController.php
@@ -6,12 +6,19 @@ use App\Http\Controllers\Controller;
 use App\Http\Controllers\ResponseTrait;
 use App\Models\Invoice;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class InvoiceController extends Controller
 {
     use ResponseTrait;
+    protected object $model;
 
-    public function index() 
+    public function __construct()
+    {
+        $this->model = Invoice::query();
+    }
+
+    public function index(Request $request) 
     {
         //query raw
         // $invoices = Invoice::select('invoices.id', 'invoices.created_at', 'invoices.user_id', 'tables.name as table_name', 'users.name as user_name')
@@ -25,31 +32,98 @@ class InvoiceController extends Controller
         //                 ->orderBy('invoices.created_at', 'desc')
         //                 ->paginate(15);
 
-        //eager loading
-        $invoices = Invoice::with(['details' => function($query) {
-                $query->select('invoice_id', 'menu_item_id', 'quantity');
-            }, 'details.menuItems' => function($query) {
-                $query->select('id', 'name','price');
-            }, 'tables' => function($query) {
-                $query->select('id', 'name');
-            }, 'users' => function($query) {
-                $query->select('id', 'name');
-            },
-        ])
-        ->orderBy('created_at', 'desc')
-        ->paginate(15);
         
+
+        //eager loading
+        // $invoices = Invoice::with(['details' => function($query) {
+        //         $query->select('invoice_id', 'menu_item_id', 'quantity');
+        //     }, 'details.menuItems' => function($query) {
+        //         $query->select('id', 'name','price');
+        //     }, 'tables' => function($query) {
+        //         $query->select('id', 'name');
+        //     }, 'users' => function($query) {
+        //         $query->select('id', 'name');
+        //     },
+        // ])
+        // ->orderBy('created_at', 'desc')
+        // ->paginate(15);
         // dd($invoices);
         // foreach ($invoices as $invoice) {
         //     dd($invoice);
+        //     dd($invoice->details->menuItems);
         //     $details = $invoice->details;
         //     foreach($details as $detail)
         //     {
         //         dd($detail->menuItems);
         //     }
         // }
+        $selected_sort_total_price = $request->sort_total_price;
+        $selected_sort_date = $request->sort_date;
+
+        $query = $this->model->newQuery()
+                ->with(['details' => function($query) {
+                    $query->select('invoice_id', 'menu_item_id', 'quantity');
+                }, 'details.menuItems' => function($query) {
+                    $query->select('id', 'name','price');
+                }, 'tables' => function($query) {
+                    $query->select('id', 'name');
+                }, 'users' => function($query) {
+                    $query->select('id', 'name');
+                },
+            ]);
+            // ->orderBy('invoices.created_at', 'desc');
+
+        // if(!is_null($selected_sort))
+        // {
+        //     if($selected_sort == 'asc' || $selected_sort == 'desc')
+        //     {
+        //         $query->orderBy('total_price', $selected_sort);
+        //     }
+        // }
+        // if (!is_null($selected_sort) && in_array($selected_sort, ['asc', 'desc'])) {
+        //     $query->join('invoice_details', 'invoices.id', '=', 'invoice_details.invoice_id')
+        //           ->join('menu_items', 'invoice_details.menu_item_id', '=', 'menu_items.id')
+        //           ->orderBy('invoices.total_price', $selected_sort)
+        //           ->select('invoices.*'); 
+        // }
+        
+        // if (!is_null($selected_sort_total_price) && in_array($selected_sort_total_price, ['asc', 'desc'])) {
+        //     // Log::info('Applying sort order:', ['column' => 'total_price', 'order' => $selected_sort]);
+        //     $query->orderBy('total_price', $selected_sort_total_price);
+        // } else {
+        //     $query->orderBy('created_at', 'desc');
+        // }
+
+        // if (!is_null($selected_sort_total_price) && in_array($selected_sort_total_price, ['asc', 'desc'])) {
+        //     $query->orderBy('total_price', $selected_sort_total_price);
+        // }else {
+        //     $query->orderBy('created_at', 'desc');
+        // }
+
+        // if (!is_null($selected_sort_date) && in_array($selected_sort_date, ['asc', 'desc'])) {
+        //     $query->orderBy('created_at', $selected_sort_date);
+        // } else {
+        //     $query->orderBy('created_at', 'desc');
+        // }
+        $appends = [];
+        if (!is_null($selected_sort_total_price) && $selected_sort_total_price != 'none' && in_array($selected_sort_total_price, ['asc', 'desc'])) {
+            $query->orderBy('total_price', $selected_sort_total_price);
+            $appends['sort_total_price'] = $selected_sort_total_price;
+        } elseif (!is_null($selected_sort_date) && $selected_sort_date != 'none' && in_array($selected_sort_date, ['asc', 'desc'])) {
+            $query->orderBy('created_at', $selected_sort_date);
+            $appends['sort_date'] = $selected_sort_date;
+        } else {
+            // $query->orderBy('created_at', 'desc');
+            $query->orderBy('id', 'desc');
+        }
+
+        // $invoices = $query->paginate(15)->appends($request->all());
+        $invoices = $query->paginate(15)->appends($appends);
+        // dd($invoices);
         return view('admin.invoice.index',[
             'invoices' => $invoices,
+            'selected_sort_total_price' => $selected_sort_total_price,
+            'selected_sort_date' => $selected_sort_date,
         ]);
     }
 }
