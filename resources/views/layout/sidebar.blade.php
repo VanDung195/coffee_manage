8dbadf2d211af52dd49b00a48b4eed57103dc733
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-07-29 23:12:19 +0700

    Update sidebar.blade.php

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -84,6 +84,12 @@
                     </a>
                 </li>
                 <li class="side-nav-item">
+                    <a href="{{ route('admin.shifts.index') }}" class="side-nav-link">
+                        <i class="uil-invoice"></i>
+                        <span> Quản lý ca làm việc </span>
+                    </a>
+                </li>
+                <li class="side-nav-item">
                     <a href="javascript: void(0);" class="side-nav-link" aria-expanded="false">
                         <i class="uil-chart-growth"></i>
                         <span> Thống kê doanh thu </span>
