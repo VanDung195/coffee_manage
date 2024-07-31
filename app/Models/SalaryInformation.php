de641740a43a6e65312ff02677d157c95c961ff7
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-07-31 21:40:19 +0700

    Update SalaryInformation.php

--- app/Models/SalaryInformation.php
+++ app/Models/SalaryInformation.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -18,4 +19,41 @@ class SalaryInformation extends Model
         'created_at',
         'updated_at',
     ];
+
+    public function getCreatedAtFormattedAttribute()
+    {
+        return date('d-m-Y', strtotime($this->created_at));
+    }
+
+    public function getPayrollDateFormattedAttribute()
+    {
+        if($this->payrolll_date)
+        {
+            return $this->payroll_date;
+        }
+        return 'Chưa';
+    }
+
+    public function getWorkingNumberAttribute()
+    {
+        // return $this->created_at->diffInDays(Carbon::now());
+        if($this->work_hours)
+        {
+            return $this->work_hours / 4;
+        }
+        return 'Chưa';
+    }
+
+    public function getAbsentNumberAttribute()
+    {
+        $work_days = $this->work_hours / 4;
+        $total_day = $this->created_at->diffInDays(Carbon::now());
+
+        return $total_day - $work_days;
+    }
+
+    public function getSalaryFormattedAttribute()
+    {
+        return number_format($this->total_amount, 0, ',', '.');
+    }
 }
