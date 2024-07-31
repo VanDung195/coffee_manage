fa7e45166e24eae3bf67cb16d1bdff6c7a46bc6b
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-07-31 21:56:33 +0700

    Create topbar.blade.php

---
+++ resources/views/layout_user/topbar.blade.php
@@ -0,0 +1,37 @@
+<div class="navbar-custom topnav-navbar topnav-navbar-dark">
+    <div class="container-fluid">
+        <!-- LOGO -->
+        <a href="" class="topnav-logo">
+            <span class="topnav-logo-lg">
+                <img src="assets/images/logo-light.png" alt="" height="16">
+            </span>
+            <span class="topnav-logo-sm">
+                <img src="assets/images/logo_sm_dark.png" alt="" height="16">
+            </span>
+        </a>
+        <ul class="list-unstyled topbar-right-menu float-right mb-0">
+            <li class="dropdown notification-list">
+                <a class="nav-link dropdown-toggle nav-user arrow-none mr-0" data-toggle="dropdown" id="topbar-userdrop" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                    <span class="account-user-avatar"> 
+                        <img src="assets/images/users/avatar-1.jpg" alt="user-image" class="rounded-circle">
+                    </span>
+                    <span>
+                        <span class="account-user-name">{{ user()->name }}</span>
+                        <span class="account-position">{{ getRoleByValue(user()->role) }}</span>
+                    </span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right dropdown-menu-animated topbar-dropdown-menu profile-dropdown" aria-labelledby="topbar-userdrop" style="">
+                    <!-- item-->
+                    <div class=" dropdown-header noti-title">
+                        <h6 class="text-overflow m-0">Welcome !</h6>
+                    </div>
+                    <!-- item-->
+                    <a href="{{ route('logout') }}" class="dropdown-item notify-item">
+                        <i class="mdi mdi-logout mr-1"></i>
+                        <span>Logout</span>
+                    </a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
