21e5c7188664789c54c4321ad9b457ef7916d59c
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-08-01 22:37:58 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -66,4 +66,5 @@ Route::get('/qr_order/{table_id}', [TableController::class, 'qr_show'])->name('q
 
 
 Route::get('/user/index', [UserController::class, 'index'])->name('user.index');
-Route::put('/user/edit/{user_id}', [UserController::class, 'edit'])->name('user.edit');
+Route::get('/user/editprofile', [UserController::class, 'edit'])->name('user.edit');
+Route::put('/user/update', [UserController::class, 'update'])->name('user.update');
