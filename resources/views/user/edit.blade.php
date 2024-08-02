22d806ce325eea27f232eb278d1185ead81060ae
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-08-02 23:19:04 +0700

    update (validation)

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -33,35 +33,49 @@
     <div class="col-sm-12">
         <div class="card">
             <div class="card-body profile-user-box">
-                <form action="{{ route('user.update') }}" method="POST">
+                <form action="{{ route('user.update') }}" method="POST" id="update-user-form">
                     @csrf
                     @method('put')
                     <div class="form-row">
                         <div class="form-group col-12 col-md-2">
                             <label for="">Tên :</label>
-                            <input type="text" name="name" id="" class="form-control" value="{{ $name }}">
-                            @if ($errors->has('name'))
+                            <input type="text" name="name" id="name" class="form-control" value="{{ $name }}">
+                            <span id="name-error" class="text-danger"></span>
+                            {{-- @if ($errors->has('name'))
                                 <span class="text-danger">{{ $errors->first('name') }}</span>
-                            @endif
+                            @endif --}}
                         </div>
                         <div class="form-group col-12 col-md-2">
                             <label for="">Ngày sinh</label>
-                            <input type="date" name="birthdate" id="" class="form-control" value="{{ $birthdate }}">
+                            <input type="date" name="birthdate" id="birthdate" class="form-control" value="{{ $birthdate }}">
+                            <span id="birthdate-error" class="text-danger"></span>
+                            {{-- @if ($errors->has('birthdate'))
+                                <span class="text-danger">{{ $errors->first('birthdate') }}</span>
+                            @endif --}}
                         </div>
                         <div class="form-group col-12 col-md-2">
                             <label for="">Số điện thoại</label>
-                            <input type="text" name="phone" id="" class="form-control" value="{{ $phone }}">
-                            @if ($errors->has('phone'))
+                            <input type="text" name="phone" id="phone" class="form-control" value="{{ $phone }}">
+                            <span id="phone-error" class="text-danger"></span>
+                            {{-- @if ($errors->has('phone'))
                                 <span class="text-danger">{{ $errors->first('phone') }}</span>
-                            @endif
+                            @endif --}}
                         </div>
                         <div class="form-group col-12 col-md-2">
                             <label for="">CCCD</label>
-                            <input type="text" name="cccd" id="" class="form-control" value="{{ $cccd }}">
+                            <input type="text" name="cccd" id="cccd" class="form-control" value="{{ $cccd }}">
+                            <span id="cccd-error" class="text-danger"></span>
+                            {{-- @if ($errors->has('cccd'))
+                                <span class="text-danger">{{ $errors->first('cccd') }}</span>
+                            @endif --}}
                         </div>
                         <div class="form-group col-12 col-md-4">
                             <label for="">Địa chỉ</label>
-                            <input type="text" name="address" id="" class="form-control" value="{{ $address }}">
+                            <input type="text" name="address" id="address" class="form-control" value="{{ $address }}">
+                            <span id="address-error" class="text-danger"></span>
+                            {{-- @if ($errors->has('address'))
+                                <span class="text-danger">{{ $errors->first('address') }}</span>
+                            @endif --}}
                         </div>
                     </div>
                     <button class="btn btn-submit btn-light" style="width:100%;">Sửa</button>
@@ -71,3 +85,44 @@
     </div> 
 </div>
 @endsection
+@push('js')
+    <script>
+        // $(document).ready(function () {
+        //     $('.btn-submit').on('click', function() {
+        //         let name = $('#name').val();
+        //         let birthdate = $('#birthdate').val();
+        //         let id = $('#phone').val();
+        //         let id = $('#phone').val();
+        //     })
+        // });
+        $(document).ready(function () {
+            $('#update-user-form').on('submit', function(event) {
+                event.preventDefault();
+                let form = $(this);
+                let url = form.attr('action');
+
+                $.ajax({
+                    type: "put",
+                    url: url,
+                    data: form.serialize(),
+                    // dataType: "dataType",
+                    success: function (response) {
+                        if (response.success) {
+                            $('.text-danger').html('');
+                            window.location.href = '{{ route('user.index') }}';
+                        }
+                    },
+                    error: function(xhr) {
+                        // console.log(xhr);
+                        var errors = xhr.responseJSON.errors;
+                        $('.text-danger').html('');
+                        for (var field in errors) {
+                            var errorHtml = errors[field][0];
+                            $('#'+field+'-error').html(errorHtml);
+                        }
+                    } 
+                });
+            })
+        });
+    </script>
+@endpush
