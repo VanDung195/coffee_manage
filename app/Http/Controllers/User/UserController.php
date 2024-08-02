b30d02163477f721f22fa00ec901cca781fed2a5
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-08-02 23:18:38 +0700

    update (update user)

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -10,10 +10,12 @@ use App\Models\Shift;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
+use PhpParser\Node\Stmt\TryCatch;
 
 class UserController extends Controller
 {
-    use ResponseTrait;
+    // use ResponseTrait;
 
     public function index()
     {
@@ -27,7 +29,8 @@ class UserController extends Controller
         // $user = user();
         $id = user()->id;
         $name = user()->name;
-        $birthdate = user()->birthdate;
+        // $birthdate = user()->birthdate;
+        $birthdate = date('d-m-Y', strtotime(user()->birthdate));;
         $gender = user()->gender ? 'Nam' : 'Nu';
         $phone = user()->phone;
         $address = user()->address;
@@ -89,48 +92,115 @@ class UserController extends Controller
         ]);
     }
 
+    // public function update(Request $request)
+    // {
+    //     // $validatedData = $request->validate([
+    //     //     'name' => 'required|string|max:255',
+    //     //     'birthdate' => 'required|date',
+    //     //     'phone' => 'required|string|max:15',
+    //     //     'cccd' => 'required|string|max:12',
+    //     //     'address' => 'required|string|max:255',
+    //     // ]);
+    //     $request->validate([
+    //         'name' => 'required',
+    //         'birthdate' => 'required|date',
+    //         'phone' => 'required|string|max:10,',
+    //         'cccd' => 'required|string|max:12',
+    //         'address' => 'required|string|max:255'
+    //     ],[
+    //         'name.required' => 'Không được để trống tên.',
+    //         'birthdate.required' => 'Không được để trống ngày sinh',
+    //         'birthdate.date' => 'Sai định dạng.',
+    //         'phone.max' => 'SDT chỉ tối đa 10 ký tự.',
+    //         'phone.string' => 'SDT không được chứa ký tự đặc biệt.',
+    //         'cccd.required' => 'Không được để trống CCCD.',
+    //         'cccd.max' => 'CCCD chỉ tối đa 12 ký tự.',
+    //         'cccd.string' => 'CCCD không được chứa ký tự đặc biệt.',
+    //         'address.required' => 'Không được để trống địa chỉ.',
+    //         'address.string' => 'Địa chỉ không được chứa ký tự đặc biệt.',
+    //         'address.max' => 'Địa chỉ chỉ tối đa 255 ký tự.',
+    //     ]); 
+
+    //     if(!auth()->check())
+    //     {
+    //         return redirect()->route('login')->with('error', 'Dang nhap da!');
+    //     }
+    //     $id = user()->id;
+    //     $name = $request->name;
+    //     $birthdate = $request->birthdate;
+    //     $phone = $request->phone;
+    //     $cccd = $request->cccd;
+    //     $address = $request->address;
+
+    //     $user = User::findOrFail($id);
+    //     $user->name = $name;
+    //     $user->birthdate = $birthdate;
+    //     $user->phone = $phone;
+    //     $user->CCCD = $cccd;
+    //     $user->address = $address;
+    //     $user->save();
+
+    //     user()->fresh();
+
+    //     return redirect()->route('user.index')->with('success', 'Cap nhat thong tin ca nhan thanh cong!!!');
+    // }
+
     public function update(Request $request)
     {
-        // $validatedData = $request->validate([
-        //     'name' => 'required|string|max:255',
-        //     'birthdate' => 'required|date',
-        //     'phone' => 'required|string|max:15',
-        //     'cccd' => 'required|string|max:12',
-        //     'address' => 'required|string|max:255',
-        // ]);
-        $request->validate([
-            'name' => 'required',
-            'birthdate' => 'required|date',
-            'phone' => 'required|string|max:10,',
-            'cccd' => 'required|string|max:12',
-            'address' => 'required|string|max:255'
-        ],[
-            'name.required' => 'Name is reiquired',
-            'birthdate.required' => 'Birthdate is required',
-            'phone.max' => 'phone max 15 characters',
-        ]); 
+        try {
+            $request->validate([
+                'name' => 'required',
+                'birthdate' => 'required|date',
+                'phone' => 'required|string|max:10,',
+                'cccd' => 'required|string|max:12',
+                'address' => 'required|string|max:255'
+            ],[
+                'name.required' => 'Không được để trống tên.',
+                'birthdate.required' => 'Không được để trống ngày sinh',
+                'birthdate.date' => 'Sai định dạng.',
+                'phone.required' => 'SDT khong duoc de trong',
+                'phone.max' => 'SDT chỉ tối đa 10 ký tự.',
+                'phone.string' => 'SDT không được chứa ký tự đặc biệt.',
+                'cccd.required' => 'Không được để trống CCCD.',
+                'cccd.max' => 'CCCD chỉ tối đa 12 ký tự.',
+                'cccd.string' => 'CCCD không được chứa ký tự đặc biệt.',
+                'address.required' => 'Không được để trống địa chỉ.',
+                'address.string' => 'Địa chỉ không được chứa ký tự đặc biệt.',
+                'address.max' => 'Địa chỉ chỉ tối đa 255 ký tự.',
+            ]); 
+    
+            if(!auth()->check())
+            {
+                // return redirect()->route('login')->with('error', 'Dang nhap da!');
+                // return 
+            }
+            $id = user()->id;
+            $name = $request->name;
+            $birthdate = $request->birthdate;
+            $phone = $request->phone;
+            $cccd = $request->cccd;
+            $address = $request->address;
+    
+            $user = User::findOrFail($id);
+            $user->name = $name;
+            $user->birthdate = $birthdate;
+            $user->phone = $phone;
+            $user->CCCD = $cccd;
+            $user->address = $address;
+            $user->save();
+    
+            user()->fresh();
+            return response()->json(['success' => true]);
+            // return redirect()->route('user.index')->with('success', 'Cap nhat thong tin ca nhan thanh cong!!!');
+        } catch (ValidationException $e) {
+            // dd($e);
+            return response()->json([
+                'success' => false,
+                'errors' => $e->errors(),
+                // errors function is return $this->validator->errors()->messages();
+            ],422);
 
-        if(!auth()->check())
-        {
-            return redirect()->route('login')->with('error', 'Dang nhap da!');
         }
-        $id = user()->id;
-        $name = $request->name;
-        $birthdate = $request->birthdate;
-        $phone = $request->phone;
-        $cccd = $request->cccd;
-        $address = $request->address;
-
-        $user = User::findOrFail($id);
-        $user->name = $name;
-        $user->birthdate = $birthdate;
-        $user->phone = $phone;
-        $user->CCCD = $cccd;
-        $user->address = $address;
-        $user->save();
-
-        user()->fresh();
-
-        return redirect()->route('user.index')->with('success', 'Cap nhat thong tin ca nhan thanh cong!!!');
+        
     }
 }
