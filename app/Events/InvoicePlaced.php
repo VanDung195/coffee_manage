1e59a23d4dd4f08c39bf1684da7b7747856b7cc2
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-08-09 23:57:13 +0700

    Update InvoicePlaced.php

--- app/Events/InvoicePlaced.php
+++ app/Events/InvoicePlaced.php
@@ -15,6 +15,7 @@ class InvoicePlaced implements ShouldBroadcast
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
     public $table_id;
+    public $table_name;
     public $details;
     public $total_price;
     public $created_at;
@@ -26,9 +27,10 @@ class InvoicePlaced implements ShouldBroadcast
     public $is_qr;
 
 
-    public function __construct($table_id, $details, $total_price, $created_at, $checkin_time, $checkout_time, $customer_payment, $remaining_money, $is_paid, $is_qr)
+    public function __construct($table_id, $table_name, $details, $total_price, $created_at, $checkin_time, $checkout_time, $customer_payment, $remaining_money, $is_paid, $is_qr)
     {
         $this->table_id = $table_id;
+        $this->table_name = $table_name;
         $this->details = $details;
         $this->total_price = $total_price;
         $this->created_at = $created_at;
@@ -38,6 +40,19 @@ class InvoicePlaced implements ShouldBroadcast
         $this->remaining_money = $remaining_money;
         $this->is_paid = $is_paid;
         $this->is_qr = $is_qr;
+
+        // session()->put('order', [
+        //     'table_id' => $this->table_id,
+        //     'details' => $this->details,
+        //     'total_price' => $this->total_price,
+        //     'created_at' => $this->created_at,
+        //     'checkin_time' => $this->checkin_time,
+        //     'checkout_time' => $this->checkout_time,
+        //     'customer_payment' => $this->customer_payment,
+        //     'remaining_money' => $this->remaining_money,
+        //     'is_paid' => $this->is_paid,
+        //     'is_qr' => $this->is_qr,
+        // ]);
     }
     // public function __construct($invoice_detail)
     // {
