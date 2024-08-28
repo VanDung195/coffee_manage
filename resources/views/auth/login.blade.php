418c6e35baee391930419f7917c288d9f82a119a
Văn Dũng <124766756+VanDung195@users.noreply.github.com>
2024-08-28 22:50:54 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,24 +54,19 @@
                                         </div>
                                     </div>
                                 </div>
-
-
                                 <div class="form-group mb-0 text-center">
                                     <button class="btn btn-primary" type="submit"> Log In </button>
                                 </div>
-
                             </form>
                         </div> <!-- end card-body -->
                     </div>
                     <!-- end card -->
-
                     <div class="row mt-3">
                         <div class="col-12 text-center">
-                            <p class="text-muted">Thằng nào vào đây? <a href="" class="text-muted ml-1"><b>Méo cho signup đâu, thoát đê</b></a></p>
+                            <p class="text-muted">Hello<a href="" class="text-muted ml-1"><b>World</b></a></p>
                         </div> <!-- end col -->
                     </div>
                     <!-- end row -->
-
                 </div> <!-- end col -->
             </div>
             <!-- end row -->
@@ -87,7 +82,4 @@
     <!-- bundle -->
     <script src="{{ asset('js/vendor.min.js') }}"></script>
     <script src="{{ asset('js/app.min.js') }}"></script>    
-    
-
-
 </body></html>
